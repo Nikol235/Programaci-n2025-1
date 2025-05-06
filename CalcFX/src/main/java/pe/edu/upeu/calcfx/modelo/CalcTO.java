86b9a82ac7f82a633df3953677cae23e307a879e
Nikol235 <gemanikolticonalaura@gmail.com>
2025-05-06 11:54:24 -0500

    Add files via upload

---
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/modelo/CalcTO.java
@@ -0,0 +1,17 @@
+package pe.edu.upeu.calcfx.modelo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity(name = "calculadora")
+@Data
+public class CalcTO {
+    @Id
+            @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Long id;
+    String num1;
+    String num2;
+    char operador;
+    String resultado;
+}
